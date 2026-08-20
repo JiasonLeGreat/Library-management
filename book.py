ebde642c2f7374d8ec07e6ef3572afd175345f06
JiasonLeGreat <anderson.jose.ignacio@gmail.com>
2026-08-20 16:30:17 -0300

    Create book.py

---
+++ book.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, author, title, book_id, is_borrowed):
+        self.author = author
+        self.title = title
+        self.book_id = book_id
+        self.is_borrowed = is_borrowed
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "id": self.book_id,
+            "is_borrowed": self.is_borrowed
+        }
