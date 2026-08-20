060424ec7b1795a703a37da7d2272e29be6525c1
JiasonLeGreat <anderson.jose.ignacio@gmail.com>
2026-08-20 16:42:16 -0300

    Update library.py

--- library.py
+++ library.py
@@ -20,7 +20,7 @@ class LibraryManager():
         with open(self.filename, 'w', encoding='utf-8') as file:
             json.dump(self.books, file, indent=4)
 
-    def add_book(self, author: str, title: str, book_id: int, is_borrowed: Boolean):
+    def add_book(self, author: str, title: str, book_id: int, is_borrowed: bool):
         if any(book['id'] == book_id for book in self.books):
             print(f"❌ Error: A book with id {id} already exists.")
             return False
