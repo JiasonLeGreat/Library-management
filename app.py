54f6ff0a81353e9857e6b91213a1c307a7dbf4da
JiasonLeGreat <anderson.jose.ignacio@gmail.com>
2026-08-20 16:28:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,52 @@
 from library import LibraryManager
 from menu import menu
 
-
 def main():
     manager = LibraryManager()
     count = 0
     while True:
         if count == 0:        
-         print("\nWelcome to the Library Management System")
-         menu()
-         count += 1
+            print("\nWelcome to the Library Management System")
+            menu()
+            count += 1
         else:
-         menu()
+            print("\nLibrary Managment System")
+            menu()
 
         choice = input("Choose an option from 1-5 ").strip()
 
         if choice == '1':
-
-            title = input("Enter book title: ").strip()
-            author = input("Enter book author: ").strip()
-            id = input("Enter unique id: ").strip()
-            if title and author and id:
-                manager.add_book(title, author, id)
-            else:
-                print("❌ All fields are required.")
+            try:
+               title = str(input("Enter book title: ").strip())
+               author = str(input("Enter book author: ").strip())
+               id = int(input("Enter unique id: "))
+               if title and author and id:
+                    manager.add_book(author, title, id, False)
+               else:
+                    print("❌ All fields are required.")
+            except ValueError:
+               print("Invalid Id, numbers only")
 
         elif choice == '2':
-           
-           manager.view_books()
+            manager.view_books()
 
         elif choice == '3':
-
-            id = input("Enter ISBN of the book to borrow: ").strip()
-            manager.borrow_book(id)
+            try:
+                id = int(input("Enter Id of the book to borrow: "))
+                manager.borrow_book(id)
+            except ValueError:
+                print("Invalid Id, numbers only")
         
         elif choice == '4':
-
-            id = input("Enter Id of the book to return: ").strip()
-            manager.return_book(id)
+            try:
+                id = int(input("Enter Id of the book to return: "))
+                manager.return_book(id)
+            except ValueError:
+                print("Invalid Id, numbers only")
 
         elif choice == "5":
             print("Goodbye!")
             break
-
         else:
             print('Invalid option, pick again')
 
