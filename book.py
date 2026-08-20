2a3f317fa61ab128e878f8262267b64337d4b9e8
JiasonLeGreat <anderson.jose.ignacio@gmail.com>
2026-08-20 16:36:57 -0300

    Update book.py

--- book.py
+++ book.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, author, title, book_id, is_borrowed):
+    def __init__(self, author: str, title: str, book_id: str, is_borrowed: Boolean):
         self.author = author
         self.title = title
         self.book_id = book_id
