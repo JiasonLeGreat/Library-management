96ae5168e365e5bbeadccb5100cd42aee50bad0a
JiasonLeGreat <anderson.jose.ignacio@gmail.com>
2026-08-20 16:29:24 -0300

    Update library.py

--- library.py
+++ library.py
@@ -1,7 +1,8 @@
 import json
 import os
+from book import Book
 
-class LibraryManager:
+class LibraryManager():
     def __init__(self, filename="library_data.json"):
         self.filename = filename
         self.books = self._load_data()
@@ -19,18 +20,13 @@ class LibraryManager:
         with open(self.filename, 'w', encoding='utf-8') as file:
             json.dump(self.books, file, indent=4)
 
-    def add_book(self, title: str, author: str, id: int):
-        if any(book['id'] == id for book in self.books):
+    def add_book(self, author, title, book_id, is_borrowed):
+        if any(book['id'] == book_id for book in self.books):
             print(f"❌ Error: A book with id {id} already exists.")
             return False
         
-        new_book = {
-            "title": title,
-            "author": author,
-            "id": id,
-            "is_borrowed": False
-        }
-        self.books.append(new_book)
+        new_book = Book(author, title, book_id, is_borrowed)
+        self.books.append(new_book.to_dict())
         self._save_data()
         print(f"✅ Success: '{title}' added to the library.")
         return True
@@ -45,7 +41,7 @@ class LibraryManager:
             status = "Borrowed" if book['is_borrowed'] else "Available"
             print(f"{i}. Title: {book['title']} | Author: {book['author']} | Id: {book['id']} | Status: [{status}]")
 
-    def borrow_book(self, id: int):
+    def borrow_book(self, id: str):
         for book in self.books:
             if book['id'] == id:
                 if book['is_borrowed']:
@@ -57,7 +53,7 @@ class LibraryManager:
                 return
         print("❌ Error: Book with that Id not found.")
 
-    def return_book(self, id: int):
+    def return_book(self, id: str):
         for book in self.books:
             if book['id'] == id:
                 if not book['is_borrowed']:
